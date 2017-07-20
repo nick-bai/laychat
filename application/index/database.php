659870b3422eb4fbe193dc1d4ca348c239d8a441
nick-bai <876337011@qq.com>
2017-07-20 18:13:00 +0800

    添加layim资源

--- application/index/database.php
+++ application/index/database.php
@@ -5,7 +5,7 @@ return [
     // 服务器地址
     'hostname'       => '127.0.0.1',
     // 数据库名
-    'database'       => 'snake',
+    'database'       => 'laychat',
     // 用户名
     'username'       => 'root',
     // 密码
